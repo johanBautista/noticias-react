9d56e0bf1deed0896e6849992d792551bbcbf670
johan Bautista <jsbparra@gmail.com>
2020-05-16 17:56:33 +0200

    llamada API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
   const [categoria, guardarCategoria] = useState('');
-  console.log(categoria);
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=7f1b7d0ae7584ed9a712c25a2e63b6ac`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
