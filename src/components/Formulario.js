01f94b2772c5528a348bda8890cc5f3b4dd33ebc
johan Bautista <jsbparra@gmail.com>
2020-05-16 17:07:57 +0200

    coustom hook y comunicacion con Form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import useSelect from '../hooks/useSelect';
 
 const Boton = styled.input`
-  margin-top: 20px;
+  /* margin-top: 10px; */
   margin-left: 20px;
   font-weight: bold;
   font-size: 20px;
-  padding: 10px;
+  padding: 13px;
   background-color: #004e92;
   border: none;
   width: 40%;
@@ -21,6 +22,19 @@ const Boton = styled.input`
 `;
 
 const Formulario = () => {
+  const Opciones = [
+    { value: 'genera', label: 'General' },
+    { value: 'bussiness', label: 'Negocios' },
+    { value: 'health', label: 'Salud' },
+    { value: 'sports', label: 'Deportes' },
+    { value: 'technology', label: 'Tecnología' },
+    { value: 'science', label: 'Ciencia' },
+    { value: 'entertainment', label: 'Entretenimiento' },
+  ];
+  console.log(Opciones);
+
+  const [categoria, SelectNoticias] = useSelect('general', Opciones);
+
   const buscarNoticias = (e) => {
     e.preventDefault();
     console.log('hola');
@@ -30,7 +44,7 @@ const Formulario = () => {
     <div className="row">
       <div className="col s12 m8 offset-m2">
         <form className="form" onSubmit={buscarNoticias}>
-          <input type="text" />
+          <SelectNoticias />
           <Boton className="" type="submit" value="BUSCAR" />
         </form>
       </div>
